770ad7cbb7c8325718441983ed268a559626d688
Gaurab Sedhai <53914680+Gourav-Sedhai@users.noreply.github.com>
2020-04-27 15:19:36 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,9 @@ data = json.load(open("data.json"))
 
 window = Tk()
 
-window.configure(bg='#856ff8')
+window.configure(bg='#81de90')
+
+window.title("Dictionary")
 
 def dictionary():
     word = e1.get()
@@ -36,18 +38,24 @@ def dictionary():
             #     txt.insert(END, data[word.upper()]) 
             # else:
             #     window.destroy()
+def clear():
+    e1.delete("0", END)
+    txt.delete("1.0", END)
 
-l1 = Label(window, text="Enter the word: ", bg='#856ff8', fg="olive")
+l1 = Label(window, bd=10, font="Times 15", text="Enter the word: ", bg='#81de90', fg="#000000")
 l1.grid(row=0, column=0)
 
 text=StringVar()
-e1 = Entry(window, textvariable=text, width=30, fg="olive", bg='#856ff8')
+e1 = Entry(window, textvariable=text, width=30, bd=5, fg="#000000", bg='#81de90')
 e1.grid(row=0, column=1)
 
-b1 = Button(window, text="Search", width=25, command=dictionary, fg="olive")
+b1 = Button(window, bd=5, font="Times 10 bold", text="Search", width=25, command=dictionary, fg="#000000")
 b1.grid(row=1, column=1)
 
-txt=Text(window, height=11, width=50, wrap=WORD, fg="olive", bg='#856ff8')
+b2 = Button(window, bd=5, font="Times 10 bold", text="Clear", width=25, command=clear, fg="#000000")
+b2.grid(row=1, column=0)
+
+txt=Text(window, height=11, width=50, wrap=WORD, fg="#000000", bg='#81de90')
 txt.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1=Scrollbar(window)
